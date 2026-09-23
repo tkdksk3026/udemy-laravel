2f11c66c6831fc3503b11ba7c485c1e871a2111f
tkdksk3026 <tkdksk3026@gmail.com>
2026-09-24 02:10:09 +0900

    section2

--- routes/web.php
+++ routes/web.php
@@ -3,5 +3,14 @@
 use Illuminate\Support\Facades\Route;
 
 Route::get('/', function () {
-    return view('welcome');
+
+});
+
+Route::get('/tasks/create', function() {
+    return view('tasks.create');
+});
+
+Route::post('/tasks/create',function(){
+    $task = request('task');
+    return  $task . 'が送信されました';
 });
